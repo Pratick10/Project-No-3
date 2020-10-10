1a285de746e583e17333f581a6464c06a0389c5e
Pratick Saha <pratick.ps@gmail.com>
2020-10-10 14:04:06 +0600

    Enroll problem solved

--- insert.php
+++ insert.php
@@ -4,19 +4,23 @@
   session_start();
   include 'connection.php';
   $id=$_SESSION['id'];
-  echo $id;
-
+  echo'student_id = ' .$id;
+ echo '<br>';
  
  if(isset($_POST["languages"]))
- {   
+ {
  	  $test_id=$_POST["test_id"];
- 	  echo $test_id;
+ 	  echo 'session id =' .$test_id;
+ 	  echo '<br>';
  	  $parts = explode(',', $_POST["languages"]);
  	  $result = sizeof($parts);
-     $sub_type = $_POST['type'];
+     $parts2 = explode(',', $_POST["type"]);
+//     $result2 = sizeof($parts2);
+//     $sub_type = $_POST['type'];
  	  //echo $result;
- 	  for ($i=0; $i < $result ; $i++) { 
- 	  	$query = "INSERT INTO requestedCours(session_id,student_id,subject_id,type) VALUES ('".$test_id."','".$id."','".$parts[$i]."','".$sub_type."')";
+ 	  for ($i=0; $i < $result; $i++) {
+ 	  	$query = "INSERT INTO requestedCours(session_id,student_id,subject_id,type) VALUES 
+            ('".$test_id."','".$id."','".$parts[$i]."','".$parts2[$i]."')";
       	$results = mysqli_query($conn,$query);
 
  	  }
--- requestCourse.php
+++ requestCourse.php
@@ -14,8 +14,8 @@ include'student_header.php';
                     <li class="breadcrumb-item active">Request For Enroll</li>
                 </ol>
                 <div>
-                    <div class="col-8">
-                        <select class="from-control" id="test" name="">
+                    <div class="form-control" align="center">
+                        <select class="from-control col-6" id="test" name="">
                             <option value=""> Select Option</option>
                             <?php
                             $str="SELECT * FROM session";
@@ -46,21 +46,35 @@ include'student_header.php';
                     $str="SELECT subject.*, section.* FROM subject,section";
                     $results = mysqli_query($conn,$str);
                     while($row = mysqli_fetch_array($results)){
-                        $section_id=$row['id'];
+                            $section_id=$row['id'];
                             $section_name="SELECT * FROM section where id=$section_id";
                             $results1 = mysqli_query($conn,$section_name);
                             $row1 = mysqli_fetch_array($results1);
-                        ?> 
-                          
+                        ?>
                           <tr>
                         <td><label><?php echo $row['sub_name']?></label></td>
                         <td><label><?php echo $row1['section']?></label></td>
-                              <td><select class="form-control " id="type" name="sub_type">
-                                      <option value="">Select type</option>
-                                      <option value="retake">Retake</option>
-                                      <option value="regular">Regular</option>
-                                      <option value="recourse">Recourse</option>
+<!--                              <td><select class="form-control ">-->
+<!--                                      <option name="type" id="type" value="">Select type</option>-->
+<!--                                      <option name="type" id="type" value="retake">Retake</option>-->
+<!--                                      <option name="type" id="type" value="regular">Regular</option>-->
+<!--                                      <option name="type" id="type" value="recourse">Recourse</option>-->
+<!--                                  </select>-->
+<!--                              </td>-->
+                              <td>
+                                  <select class="form-control id="type" name="name[]">
+
+                                      <option>Select Type</option>
+                                      <?php
+                                      $str="SELECT * FROM type";
+                                      $results2 = mysqli_query($conn,$str);
+                                      while($row2 = mysqli_fetch_array($results2)){
+                                          ?>
+                                          <option class="get_value2" value="<?php echo $row2['id']?>">
+                                              <?php echo $row2['type_name']?></option>
+                                      <?php } ?>
                                   </select>
+
                               </td>
                      <td><input type="checkbox" class="get_value" value="<?php echo $row['id']?>" /></td></tr>
                      <?php } ?></table>                      
@@ -80,22 +94,46 @@ include'student_header.php';
             var test_id=$("#test").val();
             alert(test_id);
              $("#changetable").show();
-
+               // var type=$("#type").val();
+               // alert (type);
              $("#submit").click(function(){
                 var languages=[];
-                var type=$("#type").val();
+                // var type=$("#type").val();
+                // alert(type);
+                 var type=[];
+                 $('.get_value2').each(function(){
+                     if($(this).is(":selected"))
+                     {
+                         type.push($(this).val());
+                         //console.log(type);
+                         //alert(type);
+                     }
+
+                 });
+                 type=type.toString();
                 alert(type);
-//                  var types=[];
                 $('.get_value').each(function(){
 
                     if($(this).is(":checked"))
                     {
                         languages.push($(this).val());
+                        // type.push($(this).text());
                     }
-
                 });
-                languages=languages.toString();
+                 languages=languages.toString();
+                 alert(languages);
+                 // $('.type').each(function(){
+                 //
+                 //     if($(this).is(":selected"))
+                 //     {
+                 //         type.push($(this).val());
+                 //         console.log(type);
+                 //     }
+                 //
+                 // });
 
+
+                 // alert(type);
                 $.ajax({  
                 url:"insert.php",  
                 method:"POST",  
